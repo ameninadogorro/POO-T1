e5765e3c1b908b9cea32758907a468cc5bafd971
Ana Guimaraes <ana.guimaraes63@aluno.ifce.edu.br>
2023-04-24 21:30:17 -0300

    questoes finais

---
+++ ListaProposta/q3/src/br/edu/principal/Principal.java
@@ -0,0 +1,17 @@
+package br.edu.principal;
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double num1 = 0;
+		double num2 = 0;
+		System.out.println ("Digite o primeiro número da sua divisão: ");
+		num1 = sc.nextInt();
+		System.out.println ("Digite o segundo número (diferente de 0) da sua divisão: ");
+		num2 = sc.nextInt();
+		double div = num1/num2;
+		System.out.println("O resultado da diviisão é: " + div);
+
+	}
+}
